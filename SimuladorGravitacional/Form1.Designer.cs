5997a4fd2eec305a5bd263a615145b82c7488d59
thiag <thiag@DESKTOP-12S09L8>
2024-10-04 10:48:15 -0300

    Adicionar arquivos de projeto.

---
+++ SimuladorGravitacional/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace SimuladorGravitacional
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            groupBox1 = new GroupBox();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(61, 4);
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(12, 338);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(776, 100);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(6, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(45, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Corpos";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(6, 27);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(49, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ContextMenuStrip contextMenuStrip1;
+        private GroupBox groupBox1;
+        private TextBox textBox1;
+        private Label label1;
+    }
+}
